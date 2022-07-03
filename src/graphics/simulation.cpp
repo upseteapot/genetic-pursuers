7ea759c7b92be22d1aa0045ff94e27fc6170c41d
Alfredo José <alfaadeveloper@gmail.com>
2022-07-03 20:40:51 -0300

    Update color pallet.

--- src/graphics/simulation.cpp
+++ src/graphics/simulation.cpp
@@ -3,13 +3,13 @@
 
 void Simulation::setup()
 {
-  m_target.setup(Vec2f(0.0f, 400.0f), sf::Color::Red);
+  m_target.setup(Vec2f(0.0f, 400.0f), sf::Color(250, 100, 80));
 
   m_selector.setup_shape(m_pursuer_size, m_simulation_time / m_pursuer_cooldown);
   
   m_pursuers.resize(m_pursuer_size);
   for (std::size_t i=0; i < m_pursuer_size; i++) {
-    m_pursuers[i].setup(Vec2f(0.0f, -400.0f), sf::Color(0.0f, 255.0f, 0.0f, 50.0f), m_pursuer_cooldown);
+    m_pursuers[i].setup(Vec2f(0.0f, -400.0f), sf::Color(42, 157, 143, 90), m_pursuer_cooldown);
     m_pursuers[i].reset();
   }
 
@@ -19,7 +19,7 @@ void Simulation::run(float dt)
 { 
   if (!m_paused) {
     if ((m_counter += dt) >= m_simulation_time) {
-      m_pursuers[0].set_color(sf::Color(255.0f, 255.0f, 0.0f));
+      m_pursuers[0].set_color(sf::Color(30, 255, 122));
       m_counter = 0.0f;
 
       for (std::size_t i=0; i < m_pursuer_size; i++) {
@@ -35,12 +35,12 @@ void Simulation::run(float dt)
       m_pursuers[i].update(dt, m_selector.get_genome(i));
   }
 
-  m_renderer.clear(sf::Color::Black);
-  m_renderer.draw(m_target);
+  m_renderer.clear(sf::Color(20, 22, 33));
 
   for (std::size_t i=1; i < m_pursuer_size; i++)
       m_renderer.draw(m_pursuers[i]);
   m_renderer.draw(m_pursuers[0]);
+  m_renderer.draw(m_target);
   
   m_renderer.display();
 }
